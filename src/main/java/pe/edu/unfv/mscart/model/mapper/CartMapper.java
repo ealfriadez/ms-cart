292ac9c0eabd624a438d7260a403cd8c08614dec
unknown <Eleazar_Alfredo_Alfriadez_Yriarte>
2026-09-10 12:29:45 -0500

    avanzando avanzando

--- src/main/java/pe/edu/unfv/mscart/model/mapper/CartMapper.java
+++ src/main/java/pe/edu/unfv/mscart/model/mapper/CartMapper.java
@@ -2,10 +2,16 @@ package pe.edu.unfv.mscart.model.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
+import org.mapstruct.Named;
 import org.mapstruct.ReportingPolicy;
+import pe.edu.unfv.mscart.model.dto.CartItemResponseDto;
 import pe.edu.unfv.mscart.model.dto.CartResponseDto;
+import pe.edu.unfv.mscart.model.dto.ProductResponseDto;
 import pe.edu.unfv.mscart.model.entity.CartEntity;
+import pe.edu.unfv.mscart.model.entity.CartItemEntity;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface CartMapper {
@@ -14,4 +20,23 @@ public interface CartMapper {
     @Mapping(source = "entity.creationDate", target = "creationDate")
     @Mapping(source = "entity.items", target = "items", qualifiedByName = "entityToResponseForItems")
     CartResponseDto entityToResponse(CartEntity entity);
+
+    @Mapping(source = "entity.productId", target = "productId")
+    @Mapping(source = "entity.name", target = "name")
+    @Mapping(source = "entity.price", target = "price")
+    @Mapping(source = "entity.quantity", target = "quantity")
+    @Mapping(source = "entity.subTotal", target = "subTotal")
+    @Mapping(source = "entity.creationDate", target = "creationDate")
+    CartItemResponseDto entityToResponse(CartItemEntity entity);
+
+    @Named("entityToResponseForItems")
+    default List<CartItemResponseDto> entityToResponseForItems(List<CartItemEntity> items) {
+        return items.stream().map(p -> entityToResponse(p)).collect(Collectors.toList());
+    }
+
+    @Mapping(source = "response.id", target = "id")
+    @Mapping(source = "response.name", target = "name")
+    @Mapping(source = "response.price", target = "price")
+    @Mapping(source = "quantity", target = "quantity")
+    CartItemEntity responseToEntity(ProductResponseDto response, Integer quantity);
 }
--- src/main/java/pe/edu/unfv/mscart/service/CartService.java
+++ src/main/java/pe/edu/unfv/mscart/service/CartService.java
@@ -3,9 +3,15 @@ package pe.edu.unfv.mscart.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import pe.edu.unfv.mscart.model.mapper.CartMapper;
+import pe.edu.unfv.mscart.repository.CartRepository;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class CartService {
+
+    private final CartRepository cartRepository;
+
+    private final CartMapper cartMapper;
 }
