115643105cd8a819f331378465880f88a1247645
unknown <Eleazar_Alfredo_Alfriadez_Yriarte>
2026-09-15 12:03:27 -0500

    seguimos en la Sesion 3

--- src/main/java/pe/edu/unfv/mscart/controller/CartController.java
+++ src/main/java/pe/edu/unfv/mscart/controller/CartController.java
@@ -2,6 +2,9 @@ package pe.edu.unfv.mscart.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,12 +13,20 @@ import pe.edu.unfv.mscart.model.dto.CartRequestDto;
 import pe.edu.unfv.mscart.model.dto.CartResponseDto;
 import pe.edu.unfv.mscart.service.CartService;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RequestMapping(path = "v1")
 @RestController
 @RequiredArgsConstructor
 public class CartController {
 
     private final CartService cartService;
+
+    @Value("${configuration.texto}")
+    private String text;
+
+    private final Environment env;
 
     @GetMapping(value = "/{customerId}", produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_PROBLEM_JSON_VALUE })
     public ResponseEntity<CartResponseDto> findByCustomerId(@PathVariable Long customerId) {
@@ -35,5 +46,19 @@ public class CartController {
     public ResponseEntity<CartResponseDto> removeItemFromCart(@PathVariable Long customerId,  @Valid @RequestBody CartRequestDeleteDto cartRequestDeleteDto) {
         CartResponseDto cartResponseDto = cartService.removeItem(customerId, cartRequestDeleteDto);
         return ResponseEntity.ok(cartResponseDto);
+    }
+
+    @GetMapping("/configuration")
+    public ResponseEntity<?> getConfiguration(@Value("${server.port}") String port) {
+        Map<String,String> json = new HashMap<>();
+        json.put("texto", text);
+        json.put("port", port);
+
+        if(env.getActiveProfiles().length>0 && env.getActiveProfiles()[0].equals("dev")){
+            json.put("autor.nombre", env.getProperty("configuration.autor.nombre"));
+            json.put("autor.email", env.getProperty("configuration.autor.email"));
+        }
+
+        return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
     }
 }
